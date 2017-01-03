c0188029ca52ef4e375286608bd462b9f46422c1
Bishal Napit <mebishalnapit@gmail.com>
2017-01-03 11:10:01 +0545

    Add support for header media added in WordPress 4.7

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -14,18 +14,19 @@
  * Setup the WordPress core custom header feature.
  */
 function colornews_custom_header_setup() {
-   add_theme_support( 'custom-header', apply_filters( 'colornews_custom_header_args', array(
-      'default-image'          => '',
-      'header-text'            => '',
-      'default-text-color'     => '',
-      'width'                  => 1400,
-      'height'                 => 400,
-      'flex-width'             => true,
-      'flex-height'            => true,
-      'wp-head-callback'       => '',
-      'admin-head-callback'    => '',
-      'admin-preview-callback' => 'colornews_admin_header_image',
-   ) ) );
+	add_theme_support( 'custom-header', apply_filters( 'colornews_custom_header_args', array(
+		'default-image'				=> '',
+		'header-text'				=> '',
+		'default-text-color'		=> '',
+		'width'						=> 1400,
+		'height'					=> 400,
+		'flex-width'				=> true,
+		'flex-height'				=> true,
+		'wp-head-callback'			=> '',
+		'admin-head-callback'		=> '',
+		'video'						=> true,
+		'admin-preview-callback'	=> 'colornews_admin_header_image',
+	) ) );
 }
 add_action( 'after_setup_theme', 'colornews_custom_header_setup' );
 
@@ -33,13 +34,16 @@ if ( ! function_exists( 'colornews_admin_header_image' ) ) :
 /**
  * Custom header image markup displayed on the Appearance > Header admin panel.
  */
-function colornews_admin_header_image() {
-?>
-   <div id="headimg">
-      <?php if ( get_header_image() ) : ?>
-      <img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>">
-      <?php endif; ?>
-   </div>
-<?php
+function colornews_admin_header_image() { ?>
+	<div id="headimg">
+		<?php if ( function_exists( 'the_custom_header_markup' ) ) {
+			the_custom_header_markup();
+		} else {
+			if ( get_header_image() ) : ?>
+				<img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>">
+			<?php endif;
+		} ?>
+	</div>
+	<?php
 }
 endif; // colornews_admin_header_image
--- inc/functions.php
+++ inc/functions.php
@@ -419,20 +419,19 @@ if ( ! function_exists( 'colornews_render_header_image' ) ) :
  * Shows the small info text on top header part
  */
 function colornews_render_header_image() {
-	$header_image = get_header_image();
-	if( !empty( $header_image ) ) {
-		if(get_theme_mod('colornews_header_image_link', 0) == 1) {
-		?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-		<?php
-		}
-		?>
-		<div class="header-image-wrap"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></div>
-	<?php
-		if(get_theme_mod('colornews_header_image_link', 0) == 1) {
-		?>
-		</a>
-		<?php
+	if ( function_exists( 'the_custom_header_markup' ) ) {
+			the_custom_header_markup();
+	} else {
+		$header_image = get_header_image();
+		if( ! empty( $header_image ) ) {
+			if ( get_theme_mod( 'colornews_header_image_link', 0 ) == 1 ) { ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<?php } ?>
+				<div class="header-image-wrap"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></div>
+			<?php if ( get_theme_mod( 'colornews_header_image_link', 0 ) == 1 ) { ?>
+				</a>
+				<?php
+			}
 		}
 	}
 }
