d613273749b1be0ac742ef1caf7f2fcd44ed3178
Nitu Shrestha <shresthanitu984@gmail.com>
2018-08-01 12:03:21 +0545

    Required demo importer files added

---
+++ inc/demo-importer/includes/functions-demo-config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Functions for configuring demo importer.
+ *
+ * @author   ThemeGrill
+ * @category Admin
+ * @package  Importer/Functions
+ * @version  1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Setup demo importer packages.
+ *
+ * @deprecated 1.5.0
+ *
+ * @param  array $packages
+ *
+ * @return array
+ */
+function colornews_demo_importer_packages( $packages ) {
+	$new_packages = array(
+		'colornews-free' => array(
+			'name'    => esc_html__( 'ColorNews', 'colornews' ),
+			'preview' => 'https://demo.themegrill.com/colornews/',
+		),
+		'colornews-pro'  => array(
+			'name'     => esc_html__( 'ColorNews Pro', 'colornews' ),
+			'preview'  => 'https://demo.themegrill.com/colornews/',
+			'pro_link' => 'https://themegrill.com/themes/colornews/',
+		),
+	);
+
+	return array_merge( $new_packages, $packages );
+}
+
+add_filter( 'themegrill_demo_importer_packages', 'colornews_demo_importer_packages' );
