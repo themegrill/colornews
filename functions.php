da8230652e73748c8d3782bae8a33f0730c72510
ThemeGrill <milanthapa299@gmail.com>
2020-05-21 11:56:06 +0545

    Added upgrade notice and linked to functions.php

--- functions.php
+++ functions.php
@@ -222,6 +222,7 @@ if ( is_admin() ) {
 	require( COLORNEWS_ADMIN_DIR . '/class-colornews-tdi-notice.php' );
 	require( COLORNEWS_ADMIN_DIR . '/class-colornews-notice.php' );
 	require( COLORNEWS_ADMIN_DIR . '/class-colornews-welcome-notice.php' );
+	require( COLORNEWS_ADMIN_DIR . '/class-colornews-upgrade-notice.php' );
 	require( COLORNEWS_ADMIN_DIR . '/class-colornews-theme-review-notice.php' );
 }
 
